1286c0bb403cc92c63e7432afa2177d27c206f99
Sergej Sintschilin <seregpie@gmail.com>
2018-05-29 18:08:42 +0200

    +

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 export default function(supportedLocales = []) {
-	for (let i = 0, ii = navigator.languages.length; i < ii; ++i) {
-		let locale = navigator.languages[i];
-		for (;;) {
-			if (supportedLocales.includes(locale)) {
-				return locale;
+	if (supportedLocales.length > 0) {
+		for (let i = 0, ii = navigator.languages.length; i < ii; ++i) {
+			let locale = navigator.languages[i];
+			for (;;) {
+				if (supportedLocales.includes(locale)) {
+					return locale;
+				}
+				let index = locale.lastIndexOf('-');
+				if (index < 0) {
+					break;
+				}
+				locale = locale.substring(0, index);
 			}
-			let index = locale.lastIndexOf('-');
-			if (index < 0) {
-				break;
-			}
-			locale = locale.substring(0, index);
 		}
+		return supportedLocales[0];
 	}
 	return navigator.language;
 }
